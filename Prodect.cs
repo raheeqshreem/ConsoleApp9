1759b53caa201747efb7b15cef475f55395ce68f
raheeqshreem <shreemraheeq@gmail.com>
2024-11-28 19:01:15 +0200

    Add project files.

---
+++ Prodect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Prodect
+    {
+
+        public int id;
+        public string name;
+        public double price;
+        public string description;
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prodect p = new Prodect();
+            p.name = "raheeq";
+            p.id = 1;
+            p.price = 1000;
+            p.description = "opp";
+
+
+
+            Prodect A = new Prodect();
+            A.name = "raheeq";
+            A.id = 1;
+            A.price = 1000;
+            A.description = "opp";
+
+
+
+            Prodect c = new Prodect();
+            c.name = "raheeq";
+            c.id = 1;
+            c.price = 1000;
+            c.description = "opp";
+
+        }
+    }
+}
